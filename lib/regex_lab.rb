dd7757092ef3d75386d984fe8c45429825ffa155
Austin Powell <austinpowell58@gmail.com>
2018-08-06 23:37:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,24 @@
 def starts_with_a_vowel?(word)
-
+  array = word.scan(/\b[aeiou]\w*/i)
+  if array.length >= 1 
+    true
+  else 
+    false
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b+\w{5}+\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  search = text.scan(/^[A-Z].*\W$/)
+  if search
 end
 
 def valid_phone_number?(phone)
